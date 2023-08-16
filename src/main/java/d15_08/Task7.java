5d851946ead4d01b834178f917539802f2fab15d
PavleJovancic <drjovancicpavle@gmail.com>
2023-08-16 19:42:54 +0200

    Add tasks 6-7

---
+++ src/main/java/d15_08/Task7.java
@@ -0,0 +1,31 @@
+package d15_08;
+
+public class Task7 {
+    public static void main(String[] args) {
+// (Za vezbanje)Napisati program koji iscrtava formu za registraciju kao sa slike.
+
+        System.out.println("********************************************");
+        System.out.println("*                                          *");
+        System.out.println("*                Register                  *");
+        System.out.println("*             _________________________    *");
+        System.out.println("*  Email:    |                         |   *");
+        System.out.println("*             *************************    *");
+        System.out.println("*                                          *");
+        System.out.println("*             _________________________    *");
+        System.out.println("*  Password: |                       <>|   *");
+        System.out.println("*             *************************    *");
+        System.out.println("*                                          *");
+        System.out.println("*                                          *");
+        System.out.println("    _                                      *");
+        System.out.println("*  | | I Agree                             *");
+        System.out.println("*                                          *");
+        System.out.println("*                                          *");
+        System.out.println("*               |⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺|       *");
+        System.out.println("*               |    Register      |       *");
+        System.out.println("*               |⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽|       *");
+        System.out.println("*                                          *");
+        System.out.println("* ******************************************");
+
+
+    }
+}
